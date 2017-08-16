23f34bd278df21e80770691b00f6846dc145934c
Jinzhu <wosmvp@gmail.com>
2017-08-16 22:02:53 +0800

    Define Application, MiddlewareStack

--- application.go
+++ application.go
@@ -2,8 +2,16 @@ package application
 
 // Application application structure
 type Application struct {
+	Config *Config
+}
+
+// Config application config
+type Config struct {
 }
 
 // New new application
-func New() *Application {
+func New(config *Config) *Application {
+	return &Application{
+		Config: config,
+	}
 }
